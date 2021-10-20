0bbe1ee7044fbb77f2262412efc928470709ba02
Tomas Krnak <tomas@krnak.cz>
2021-10-20 16:09:03 +0200

    add std::io::Error support

--- src/error.rs
+++ src/error.rs
@@ -8,18 +8,36 @@
 // - Deirdre Connolly <deirdre@zfnd.org>
 // - Henry de Valence <hdevalence@hdevalence.ca>
 
-//use thiserror::Error;
-
-/// An error related to RedJubJub signatures.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)] // Error
-pub enum Error {
-    /// The encoding of a signing key was malformed.
-    //#[error("Malformed signing key encoding.")]
-    MalformedSigningKey,
-    /// The encoding of a verification key was malformed.
-    //#[error("Malformed verification key encoding.")]
-    MalformedVerificationKey,
-    /// Signature verification failed.
-    //#[error("Invalid signature.")]
-    InvalidSignature,
+#[cfg(feature = "std")]
+mod error {
+    use thiserror::Error;
+    extern crate std;
+    /// An error related to RedJubJub signatures.
+    #[derive(Debug, Copy, Clone, Eq, PartialEq, Error)]
+    pub enum Error {
+        /// The encoding of a signing key was malformed.
+        #[error("Malformed signing key encoding.")]
+        MalformedSigningKey,
+        /// The encoding of a verification key was malformed.
+        #[error("Malformed verification key encoding.")]
+        MalformedVerificationKey,
+        /// Signature verification failed.
+        #[error("Invalid signature.")]
+        InvalidSignature,
+    }
 }
+
+#[cfg(not(feature = "std"))]
+mod error {
+    #[derive(Debug, Copy, Clone, Eq, PartialEq)]
+    pub enum Error {
+        /// The encoding of a signing key was malformed.
+        MalformedSigningKey,
+        /// The encoding of a verification key was malformed.
+        MalformedVerificationKey,
+        /// Signature verification failed.
+        InvalidSignature,
+    }
+}
+
+pub use error::Error;
